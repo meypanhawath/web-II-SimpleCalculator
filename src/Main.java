f7e5025c7d6f4d59cf45ca3857068282f95f82e2
Raz <panhawath18@gmail.com>
2025-07-15 23:57:27 +0700

    add some emoji

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,21 @@ public class Main {
         Scanner input = new Scanner(System.in);
         float val1, val2, val3;
 
-        System.out.print("Enter Value 1: ");
+        System.out.print("👉 Enter Value 1: ");
         val1 = input.nextFloat();
-        System.out.print("Enter Value 2: ");
+        System.out.print("👉 Enter Value 2: ");
         val2 = input.nextFloat();
-        System.out.print("Enter Value 3: ");
+        System.out.print("👉 Enter Value 3: ");
         val3 = input.nextFloat();
-
+        System.out.println("=".repeat(30));
         float sum = val1 + val2 + val3;
         float sub = val1 - val2 - val3;
         float mul = val1 * val2 * val3;
         float div = val1 / val2 / val3;
 
-        System.out.println("The sum is: " + sum);
-        System.out.println("The sub is: " + sub);
-        System.out.println("The mul is: " + mul);
-        System.out.printf("The div is: %.2f" + div);
+        System.out.println("➕ The sum is: " + sum);
+        System.out.println("➖ The sub is: " + sub);
+        System.out.println("✖️ The mul is: " + mul);
+        System.out.printf("➗ The div is: %.2f", div);
     }
 }
